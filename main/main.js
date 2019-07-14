dd30db92497fff248b78165ea42606ccb5684a85
2228787638 <2228787638@qq.com>
2019-07-14 18:25:31 +0800

    feat : implement decodeBarcodes

--- main/main.js
+++ main/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 //TODO: 请在该文件中实现练习要求并删除此注释
+
+const decodeBarcodes= barcodes =>{
+    const idAndCountMap = new Map();
+	const items=new Array();
+	
+	for(const barcode of barcodes){
+		idAndCountMap.set(barcode,idAndCountMap.get(barcode)+1||1);
+	}
+			 
+	for(const idAndCount of idAndCountMap){
+        const order={};
+        if(idAndCount.indexOf('-')){
+            order.id=idAndCount[0].split('-')[0];
+            order.count=idAndCount[0].split('-')[1];
+            items.push(order);
+        }else{
+            order.id=idAndCount[0];
+            order.count=idAndCount[1];
+            items.push(order);
+        }
+	}
+	return items;
+}
